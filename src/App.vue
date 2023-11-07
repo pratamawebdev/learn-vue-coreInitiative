3f6731d80fe4bdc0a2fb1636a5223effa91f1a65
willy <willypratama231@gmail.com>
2023-11-07 14:39:25 +0700

    Create global & local components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Home />
+  <ButtonIncrease />
+  <ButtonDecrease />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import ButtonDecrease from "./components/ButtonDecrease.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    ButtonDecrease,
   },
 };
 </script>
